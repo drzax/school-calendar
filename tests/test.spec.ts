ed39e9a4e608c3ec77708828924d5cd7373e69f4
Simon Elvery <simon@elvery.net>
2022-08-24 21:27:04 +1000

    run e2e tests on deploys

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('homepage has "School Calendar" in title', async ({ page }) => {
-	await page.goto('http://localhost:4173/');
+	await page.goto('/');
 	await expect(page).toHaveTitle(/School Calendar/);
 });
 
