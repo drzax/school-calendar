9ddc05f74f7d381d4b820e47b3acda56c1b4a8ca
Simon Elvery <simon@elvery.net>
2022-08-25 10:59:51 +1000

    Fix test configs

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,7 @@
 module.exports = {
 	preset: 'ts-jest/presets/default-esm',
 	testEnvironment: 'node',
+	testPathIgnorePatterns: ['/node_modules/', '/tests/'],
 	moduleNameMapper: {
 		'^\\$lib/(.*)': '<rootDir>/src/lib/$1'
 	}
