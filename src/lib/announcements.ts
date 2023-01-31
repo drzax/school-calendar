f1e077256b1692b9e7952e1eac1399efbbce7327
Simon Elvery <simon@elvery.net>
2023-01-31 11:13:18 +1000

    Add no calendar announcement

--- src/lib/announcements.ts
+++ src/lib/announcements.ts
@@ -13,5 +13,10 @@ export const announcements: Announcement[] = [
 		from: '2022-08-25',
 		to: '2022-09-01',
 		content: `Some 🐛 bugs have been 💥 squashed. If you subscribed in your calendar software of choice, please <a href="${getSubscriptionUrl()}">re-subscribe</a> to make sure you're getting correct details.`
+	},
+	{
+		from: '2023-01-30',
+		to: '2023-02-15',
+		content: `📆 The new school year has started, but we don't yet have details of the new school calendar. As soon as the school sends out a calendar link, this should get an update. Any events below are still using last year's calendar and <strong>may be inaccurate</strong>.`
 	}
 ];
