6f94b2eb646774bb7025a602dc3e1dd06eb34c0b
Simon Elvery <simon@elvery.net>
2023-04-24 11:41:45 +1000

    🐛 Don't use ID as a unique identifier

--- src/lib/CalendarGroup.svelte
+++ src/lib/CalendarGroup.svelte
@@ -12,7 +12,7 @@
 	{#if subtitle}<h4 class="mr-3 mt-1 text-sm text-gray-500">{subtitle}</h4>{/if}
 </header>
 <div class="rounded-md my-2 bg-white shadow">
-	{#each entries as { id, start, end, title, description, categories, yearLevels, allDay, location, isNew, isUpdated } (id)}
+	{#each entries as { id, start, end, title, description, categories, yearLevels, allDay, location, isNew, isUpdated } (`${id} ${start}`)}
 		<div class="p-3 border-b flex flex-col md:flex-row justify-start">
 			<div class="md:mx-2 md:my-0 my-2 flex-1">
 				<h4 class="font-semibold text-pink-500">
